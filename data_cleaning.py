b940eed881e27ba80a69880647a25c3deba6a234
Eniola Kayode <100062989+beingEniola@users.noreply.github.com>
2026-07-20 20:11:07 +0100

    Addressed review comments

--- data_cleaning.py
+++ data_cleaning.py
@@ -7,4 +7,7 @@ def clean_data(df):
     # Fill missing values
     df = df.fillna("Unknown")
 
+    #standardize column names
+    df.columns = [col.strip().lower().replace(" ", "_") for col in df.columns]
+
     return df
